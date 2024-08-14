ed50a30414c4496a904183b34e34015741a63dda
Hicham El Malki <hi.elmalki@hotmail.com>
2024-08-14 20:23:51 +0200

    Beans Lifesycle

---
+++ src/main/java/com/example/spring/tutorial/demo/core/beans/BeanController.java
@@ -0,0 +1,30 @@
+package com.example.spring.tutorial.demo.core.beans;
+
+
+import com.example.spring.tutorial.demo.core.qualifiers.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/beans")
+public class BeanController {
+
+    private Coach cricketCoach1;
+    private Coach cricketCoach2;
+
+    @Autowired
+    public BeanController(@Qualifier("cricketCoach") Coach cricketCoach1,
+                          @Qualifier("cricketCoach") Coach cricketCoach2){
+        this.cricketCoach1 = cricketCoach1;
+        this.cricketCoach2 = cricketCoach2;
+    }
+
+    @GetMapping("/check")
+    public boolean compare(){
+        return this.cricketCoach1 == this.cricketCoach2;
+    }
+
+}
--- src/main/java/com/example/spring/tutorial/demo/core/qualifiers/CricketCoach.java
+++ src/main/java/com/example/spring/tutorial/demo/core/qualifiers/CricketCoach.java
@@ -1,14 +1,28 @@
 package com.example.spring.tutorial.demo.core.qualifiers;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
 
     public CricketCoach() {
         log.info("In constructor: {}", getClass().getSimpleName());
+    }
+
+    @PostConstruct
+    public void doMyStartupStuff() {
+        log.info("In doMyStartupStuff(): {}", getClass().getSimpleName());
+    }
+
+    // define our destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        log.info("In doMyCleanupStuff(): {}", getClass().getSimpleName());
     }
 
     @Override
